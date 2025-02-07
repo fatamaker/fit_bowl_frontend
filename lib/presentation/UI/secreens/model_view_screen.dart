8d728b95a5647d4e53bede82603e89bfe9fe8eab
fatma kerkeni <fatmakerkeni2004@gmail.com>
2025-02-07 16:04:22 +0100

    arupadte2

--- lib/presentation/UI/secreens/model_view_screen.dart
+++ lib/presentation/UI/secreens/model_view_screen.dart
@@ -8,19 +8,19 @@ class ModelViewerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('3D Model Viewer'),
-        ),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Model Viewer')),
         body: const ModelViewer(
           backgroundColor: Color.fromARGB(0xFF, 0xEE, 0xEE, 0xEE),
-          src: 'assets/Astronaut.glb',
+          src: 'https://modelviewer.dev/shared-assets/models/Astronaut.glb',
           alt: 'A 3D model of an astronaut',
           ar: true,
-          arModes: ['scene-viewer', 'webxr', 'quick-look'],
           autoRotate: true,
           iosSrc: 'https://modelviewer.dev/shared-assets/models/Astronaut.usdz',
           disableZoom: true,
-        ));
+        ),
+      ),
+    );
   }
 }
